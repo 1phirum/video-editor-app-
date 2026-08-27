194cfd3d6271a87c4170ec747bd47225d458c734
darak12345fh-collab <darak12345fh-collab@users.noreply.github.com>
2026-08-27 10:47:39 +0700

    Remove accidentally added build and dist folders

--- python-app/build-subtitle-verify/CMakeFiles/CMakeScratch/TryCompile-zy8sum/CMakeFiles/cmTC_a2f89.dir/CMakeCXXCompilerABI.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/cmTC_a2f89.dir/CMakeCXXCompilerABI.cpp.obj: \
-  C:/Program\ Files/CMake/share/cmake-4.3/Modules/CMakeCXXCompilerABI.cpp \
-  C:/Program\ Files/CMake/share/cmake-4.3/Modules/CMakeCompilerABI.h
--- python-app/build-verify/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/premier-pro/python-app
-# Build directory: C:/premier-pro/python-app/build-verify
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[cutpro_backend_tests]=] "C:/premier-pro/python-app/build-verify/bin/cutpro_backend_tests.exe")
-set_tests_properties([=[cutpro_backend_tests]=] PROPERTIES  ENVIRONMENT "CUTPRO_CACHE_DIR=C:/premier-pro/python-app/build-verify/test-cache;CUTPRO_SETTINGS_FILE=C:/premier-pro/python-app/build-verify/test-settings.ini" _BACKTRACE_TRIPLES "C:/premier-pro/python-app/CMakeLists.txt;153;add_test;C:/premier-pro/python-app/CMakeLists.txt;0;")
